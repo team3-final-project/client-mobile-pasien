7217241bd6b978e200c1b9dd0581a6411c2cd520
wellytansil <we2y.tansil@gmail.com>
2020-12-14 14:45:09 +0700

    connetinc back and front end

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Login, Home } from './screens'
+import { Provider } from 'react-redux'
+import store from './store/index'
 
 const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Login} />
-        <Stack.Screen name="Dashboard" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Welcome" component={Login} />
+          <Stack.Screen name="Dashboard" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
