73098a20d037b2fc7f89aff0ea3545678aa930c7
dickyandeyan <dickyundead@gmail.com>
2020-12-14 20:14:07 +0700

    asyncStorage

--- store/index.js
+++ store/index.js
@@ -2,65 +2,64 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 const initialState = {
-    isLoggedIn: false,
-    record: []
-}  
+  isLoggedIn: false,
+  record: []
+}
 
 export function login(input) {
-
-    return (dispatch) => {
-        fetch('http://localhost:3000/patient', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(input)
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                return Promise.reject('something went wrong!')
-            }
-        })
-        .then(data => {
-            dispatch({ type: 'login_patient', payload: data.access_token })
-        })
-    }
+  return (dispatch) => {
+    fetch('http://192.168.0.104:3000/patient', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(input)
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          return Promise.reject('something went wrong!')
+        }
+      })
+      .then((data) => {
+        dispatch({ type: 'login_patient', payload: data.access_token })
+      })
+  }
 }
 
 export function readRecord() {
-    const access_token = localStorage.getItem('access_token')
-    return (dispatch) => {
-        fetch('http://localhost:3000/patient', {
-            method: 'GET',
-            headers: {
-                access_token: access_token
-            }
-        })
-        .then(response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                return Promise.reject('something went wrong!')
-            }
-        })
-        .then(data => {
-            dispatch({ type: 'get_record', payload: data })
-        })
-    }
+  const access_token = localStorage.getItem('access_token')
+  return (dispatch) => {
+    fetch('http://192.168.0.104:3000/patient', {
+      method: 'GET',
+      headers: {
+        access_token: access_token
+      }
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          return Promise.reject('something went wrong!')
+        }
+      })
+      .then((data) => {
+        dispatch({ type: 'get_record', payload: data })
+      })
+  }
 }
 
 const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'login_patient':
-            localStorage.setItem('access_token', action.payload)
-            return {...state, isLoggedIn: true}
-        case 'get_record':
-            return { ...state, record: action.payload }
-        default:
-            return state
-    }
+  switch (action.type) {
+    case 'login_patient':
+      localStorage.setItem('access_token', action.payload)
+      return { ...state, isLoggedIn: true }
+    case 'get_record':
+      return { ...state, record: action.payload }
+    default:
+      return state
+  }
 }
 
 const store = createStore(reducer, applyMiddleware(thunk))
